0759586287b852e97bf5a8f261b82d16d3b512de
wangbingok1118 <wangbingok1118@163.com>
2018-04-10 17:53:42 +0800

    download url image mul thread

--- labelx-voc-toolkit/labexl2voc/image_helper.py
+++ labelx-voc-toolkit/labexl2voc/image_helper.py
@@ -5,6 +5,7 @@ import json
 import hashlib
 import threading
 import Queue
+import time
 
 def downloadImage_By_urllist(labelxjson=None, tempSaveDir=None, vocpath=None):
     imageSaveDir = os.path.join(vocpath, 'JPEGImages')
@@ -22,7 +23,9 @@ def downloadImage_By_urllist(labelxjson=None, tempSaveDir=None, vocpath=None):
                 print("WARNING : %s url is None"%(line))
             else:
                 w_f.write(url+'\n')
-    wgetImageFromUrl(urlFile=justUrlListFile, saveBasePath=imageSaveDir)
+    # wgetImageFromUrl(urlFile=justUrlListFile, saveBasePath=imageSaveDir)
+    wgetImageFromUrl_MulThread(
+        urlFile=justUrlListFile, saveBasePath=imageSaveDir)
 
 # wget images
 def wgetImageFromUrl(urlFile=None, saveBasePath=None):
@@ -55,19 +58,107 @@ def wgetImageFromUrl(urlFile=None, saveBasePath=None):
     pass
 
 
+GLOBAL_LOCK = threading.Lock()
+ERROR_NUMBER = 0
+IMAGE_SAVE_PATH=None
+THREAD_DOWNLOAD_COUNT = 20
 def wgetImageFromUrl_MulThread(urlFile=None, saveBasePath=None):
     # globale vars initialization
-    GLOBAL_LOCK = threading.Lock()
-    ERROR_NUMBER = 0
-    FILE_NAME = str()
-    
+    global IMAGE_SAVE_PATH
+    inputFileOp = open(urlFile,'r')
+    logErOp = open(urlFile+'-wget-error.log','w')
+    IMAGE_SAVE_PATH = saveBasePath
+    if not os.path.exists(IMAGE_SAVE_PATH):
+        os.makedirs(IMAGE_SAVE_PATH)
+    queue = Queue.Queue(0)
+    thread_prod = prod_worker(queue, inputFileOp)
+    thread_prod.start()
+    print('thread:', thread_prod.name, 'successfully started')
+    time.sleep(10)
+    for i in xrange(THREAD_DOWNLOAD_COUNT):
+        exec('thread_cons_{} = cons_worker(queue,IMAGE_SAVE_PATH,logErOp)'.format(i))
+        eval('thread_cons_{}.start()'.format(i))
+    thread_prod.join()
+    for i in xrange(THREAD_DOWNLOAD_COUNT):
+        eval('thread_cons_{}.join()'.format(i))
+    print('total error number:', ERROR_NUMBER)
+    inputFileOp.close()
+    logErOp.close()
+
     pass
 
 
+class prod_worker(threading.Thread):
+    """
+    producing worker
+    """
+    global GLOBAL_LOCK
 
-GLOBAL_LOCK = threading.Lock()
-ERROR_NUMBER = 0
-FILE_NAME = str()
+    def __init__(self, queue, infileOp):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.infileOp = infileOp
+    def run(self):
+        for line in self.infileOp:
+            line = line.strip()
+            if line == None or len(line) <=0:
+                continue
+            GLOBAL_LOCK.acquire()
+            self.queue.put(line)
+            GLOBAL_LOCK.release()
+        GLOBAL_LOCK.acquire()
+        print('thread:', self.name, 'successfully quit')
+        GLOBAL_LOCK.release()
+
+
+class cons_worker(threading.Thread):
+    global GLOBAL_LOCK
+    global IMAGE_SAVE_PATH
+    def __init__(self, queue,savePath,logErOp):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.savePath = savePath
+        self.logErOp = logErOp
+
+    def download(self, url, output_path):
+        err_flag = 0
+        image_save_path = os.path.join(output_path,url.split('/')[-1])
+        try:
+            cmdStr = "wget %s -O %s -q" % (url, image_save_path)
+            ret = os.system(cmdStr)
+            count = 5
+            while ret != 0 and count > 0:
+                ret = os.system(cmdStr)
+                count -= 1   
+            if ret != 0:
+                err_flag = 1
+        except all as e:
+            err_flag = 1
+        return err_flag
+
+    def run(self):
+        global ERROR_NUMBER
+        err_num = 0
+        while(not self.queue.empty()):
+            if GLOBAL_LOCK.acquire(False):
+                # customized downloading code
+                url = self.queue.get()
+                GLOBAL_LOCK.release()
+                err_flag = self.download(url, IMAGE_SAVE_PATH)
+                if err_flag == 1:
+                    # wget error
+                    err_num += 1
+                    GLOBAL_LOCK.acquire()
+                    self.logErOp.write(url)
+                    self.logErOp.flush()
+                    GLOBAL_LOCK.release()
+                    pass
+            else:
+                pass
+        GLOBAL_LOCK.acquire()
+        ERROR_NUMBER += err_num
+        print('thread:', self.name, 'successfully quit')
+        GLOBAL_LOCK.release()
 
 
 
